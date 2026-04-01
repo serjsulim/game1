7046f20c7856ac13c5ea8879c3781247bb050e08
serjsulim <serjsulim@gmail.com>
2026-04-01 21:16:58 +0300

    1

--- g2/main.py
+++ g2/main.py
@@ -1,2 +1,22 @@
 import pygame
+import sys
+
+
+def run():
+    width = 1000                               # ширина вікна
+    height = 600                               # висота вікна
+
+    pygame.init()
+    screen = pygame.display.set_mode((width,height)) # створюємо вікно 
+    pygame.display.set_caption('Arkanoid')           # заголовок вікна
+    bg_color = (0, 0, 0)                             # змінна колір фону вікна
+
+    while True:
+        for event in pygame.event.get():             # для всіх подій
+            if event.type == pygame.QUIT:            # якщо натиснуто хрестик на вікні 
+                sys.exit()                           #  то вихід
+        screen.fill(bg_color)                        # застосувати колір фону
+        pygame.display.flip()                        # промалювати кадр
+
+run()
 
